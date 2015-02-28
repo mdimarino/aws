cbfe93876a1cf9683acff967a6d94dd78872b9da
mazevedo <marcello@di.marino.nom.br>
2015-02-28 11:46:15 -0300

    Modificacoes no script de criacao da vpc

--- create_vpc.sh
+++ create_vpc.sh
@@ -1,92 +1,161 @@
 #!/bin/bash
 
-vpcname="vpc-02"
+vpcname=$1
+
+echo =================================================================================
 
 # Criar VPC com CIDR 10.50.0.0/16
     echo "Criando VPC $vpcname com CIDR 10.50.0.0/16"
-    vpc_id=$(aws ec2 create-vpc --cidr-block 10.50.0.0/16 --output text --query 'Vpc.VpcId')
-    echo VpcId=$vpc_id
+    vpcid=$(aws ec2 create-vpc --cidr-block 10.50.0.0/16 --output text --query 'Vpc.VpcId')
+    echo VpcId=$vpcid
 
-    echo "Alterando Tag Name"
-    aws ec2 create-tags --resources $vpc_id --tags Key=Name,Value=$vpcname
+    echo "Alterando Tag Name da vpc para '$1'"
+    aws ec2 create-tags --resources $vpcid --tags Key=Name,Value=$vpcname
 
     echo -n "Aguardando pela VPC..."
     while state=$(aws ec2 describe-vpcs --filters Name=tag-key,Values=vpcname --filters Name=tag-value,Values=$vpcname --output text --query 'Vpcs[*].State'); test "$state" = "pending"; do
         echo -n . ; sleep 3;
     done; echo " $state"
 
+echo =================================================================================
+
 # Criar um internet gateway (para permitir acesso a internet)
     echo "Criando um internet gateway"
-    igw=$(aws ec2 create-internet-gateway --output text --query 'InternetGateway.InternetGatewayId')
-    echo InternetGatewayId=$igw
+    internetgatewayid=$(aws ec2 create-internet-gateway --output text --query 'InternetGateway.InternetGatewayId')
+    echo InternetGatewayId=$internetgatewayid
 
-    echo "Alterando Tag Name"
-    aws ec2 create-tags --resources $igw --tags Key=Name,Value="Gateway da Subrede Publica"
+    echo "Alterando Tag Name do internet gateway para 'Gateway da Subrede Publica'"
+    aws ec2 create-tags --resources $internetgatewayid --tags Key=Name,Value="Gateway da Subrede Publica"
+
+echo =================================================================================
 
 # Atachar o internet gateway na vpc
-    echo "Atachando o internet gateway na vpc"
-    aws ec2 attach-internet-gateway --internet-gateway-id $igw --vpc-id $vpc_id
+    echo "Atachando o internet gateway na vpc $1"
+    aws ec2 attach-internet-gateway --internet-gateway-id $internetgatewayid --vpc-id $vpcid
+
+echo =================================================================================
 
 # Criar tabelas de roteamento para subredes publicas e privadas
     echo "Criando tabela de roteamento para subredes publicas"
     # Quando a vpc é criada uma tabela de roteamento default eh criada automaticamente, vamos usa-la para as subredes publicas
-    public_rtb_id=$(aws ec2 describe-route-tables --filters Name=vpc-id,Values=$vpc_id --output text --query 'RouteTables[*].RouteTableId')
-    echo RouteTableId=$public_rtb_id
+    public_routetableid=$(aws ec2 describe-route-tables --filters Name=vpc-id,Values=$vpcid --output text --query 'RouteTables[*].RouteTableId')
+    echo RouteTableId=$public_routetableid
 
-    echo "Alterando Tag Name"
-    aws ec2 create-tags --resources $public_rtb_id --tags Key=Name,Value="Tabela de Roteamento Publica"
+    echo "Alterando Tag Name da tabela de roteamento para 'Tabela de Roteamento Publica'"
+    aws ec2 create-tags --resources $public_routetableid --tags Key=Name,Value="Tabela de Roteamento Publica"
 
     echo "Criando tabela de roteamento para subredes privadas"
-    private_rtb_id=$(aws ec2 create-route-table --vpc-id $vpc_id --output text --query 'RouteTable.RouteTableId')
-    echo RouteTableId=$private_rtb_id
+    private_routetableid=$(aws ec2 create-route-table --vpc-id $vpcid --output text --query 'RouteTable.RouteTableId')
+    echo RouteTableId=$private_routetableid
 
-    echo "Alterando Tag Name"
-    aws ec2 create-tags --resources $private_rtb_id --tags Key=Name,Value="Tabela de Roteamento Privada"
+    echo "Alterando Tag Name da tabela de roteamento para 'Tabela de Roteamento Privada'"
+    aws ec2 create-tags --resources $private_routetableid --tags Key=Name,Value="Tabela de Roteamento Privada"
+
+echo =================================================================================
 
 # Criar rota na tabela de roteamento publica para o internet gateway, ira rotear o trafego para fora desta rede para internet
     echo "Criando rota na tabela de roteamento publica para o internet gateway"
-    aws ec2 create-route --route-table-id $public_rtb_id --gateway-id $igw --destination-cidr-block 0.0.0.0/0
+    aws ec2 create-route --route-table-id $public_routetableid --gateway-id $internetgatewayid --destination-cidr-block 0.0.0.0/0
+
+echo =================================================================================
 
 # Criar subnets:
 
 # Subrede Publica 01 - 10.50.0.0/24 - zona de disponibilidade us-east-1a
-    subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.50.0.0/24 --availability-zone us-east-1a --output text --query 'Subnet.SubnetId')
-    echo SubnetId=$subnet_id
-    echo "Alterando Tag Name"
-    aws ec2 create-tags --resources $subnet_id --tags Key=Name,Value="Subrede Publica 01"
-    aws ec2 associate-route-table --subnet-id $subnet_id --route-table-id $public_rtb_id
+    echo "Criando subrede publica 01 - 10.50.0.0/24 - us-east-1a"
+    subnetid_0=$(aws ec2 create-subnet --vpc-id $vpcid --cidr-block 10.50.0.0/24 --availability-zone us-east-1a --output text --query 'Subnet.SubnetId')
+    echo SubnetId=$subnetid_0
+    echo "Alterando Tag Name da subrede para 'Subrede Publica 01'"
+    aws ec2 create-tags --resources $subnetid_0 --tags Key=Name,Value="Subrede Publica 01"
+    echo "Associando tabela de roteamento publica $public_routetableid a subrede $subnetid_0"
+    aws ec2 associate-route-table --subnet-id $subnetid_0 --route-table-id $public_routetableid
+
+echo =================================================================================
 
 # Subrede Privada 01 - 10.50.1.0/24 - zona de disponibilidade us-east-1a
-    subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.50.1.0/24 --availability-zone us-east-1a --output text --query 'Subnet.SubnetId')
-    echo SubnetId=$subnet_id
-    echo "Alterando Tag Name"
-    aws ec2 create-tags --resources $subnet_id --tags Key=Name,Value="Subrede Privada 01"
-    aws ec2 associate-route-table --subnet-id $subnet_id --route-table-id $private_rtb_id
+    echo "Criando subrede privada 01 - 10.50.1.0/24 - us-east-1a"
+    subnetid_1=$(aws ec2 create-subnet --vpc-id $vpcid --cidr-block 10.50.1.0/24 --availability-zone us-east-1a --output text --query 'Subnet.SubnetId')
+    echo SubnetId=$subnetid_1
+    echo "Alterando Tag Name da subrede para 'Subrede Privada 01'"
+    aws ec2 create-tags --resources $subnetid_1 --tags Key=Name,Value="Subrede Privada 01"
+    echo "Associando tabela de roteamento privada $private_routetableid a subrede $subnetid_1"
+    aws ec2 associate-route-table --subnet-id $subnetid_1 --route-table-id $private_routetableid
+
+echo =================================================================================
 
 # Subrede Publica 02 - 10.50.2.0/24 - zona de disponibilidade us-east-1b
-    subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.50.2.0/24 --availability-zone us-east-1b --output text --query 'Subnet.SubnetId')
-    echo SubnetId=$subnet_id
-    echo "Alterando Tag Name"
-    aws ec2 create-tags --resources $subnet_id --tags Key=Name,Value="Subrede Publica 02"
-    aws ec2 associate-route-table --subnet-id $subnet_id --route-table-id $public_rtb_id
+    echo "Criando subrede publica 02 - 10.50.2.0/24 - us-east-1b"
+    subnetid_2=$(aws ec2 create-subnet --vpc-id $vpcid --cidr-block 10.50.2.0/24 --availability-zone us-east-1b --output text --query 'Subnet.SubnetId')
+    echo SubnetId=$subnetid_2
+    echo "Alterando Tag Name da subrede para 'Subrede Publica 02'"
+    aws ec2 create-tags --resources $subnetid_2 --tags Key=Name,Value="Subrede Publica 02"
+    echo "Associando tabela de roteamento publica $public_routetableid a subrede $subnetid_2"
+    aws ec2 associate-route-table --subnet-id $subnetid_2 --route-table-id $public_routetableid
+
+echo =================================================================================
 
 # Subrede Privada 02 - 10.50.3.0/24 - zona de disponibilidade us-east-1b
-    subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.50.3.0/24 --availability-zone us-east-1b --output text --query 'Subnet.SubnetId')
-    echo SubnetId=$subnet_id
-    echo "Alterando Tag Name"
-    aws ec2 create-tags --resources $subnet_id --tags Key=Name,Value="Subrede Privada 02"
-    aws ec2 associate-route-table --subnet-id $subnet_id --route-table-id $private_rtb_id
+    echo "Criando subrede privada 02 - 10.50.3.0/24 - us-east-1b"
+    subnetid_3=$(aws ec2 create-subnet --vpc-id $vpcid --cidr-block 10.50.3.0/24 --availability-zone us-east-1b --output text --query 'Subnet.SubnetId')
+    echo SubnetId=$subnetid_3
+    echo "Alterando Tag Name da subrede para 'Subrede Privada 02'"
+    aws ec2 create-tags --resources $subnetid_3 --tags Key=Name,Value="Subrede Privada 02"
+    echo "Associando tabela de roteamento privada $private_routetableid a subrede $subnetid_3"
+    aws ec2 associate-route-table --subnet-id $subnetid_3 --route-table-id $private_routetableid
+
+echo =================================================================================
 
 # Subrede Publica 03 - 10.50.4.0/24 - zona de disponibilidade us-east-1c
-    subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.50.4.0/24 --availability-zone us-east-1c --output text --query 'Subnet.SubnetId')
-    echo SubnetId=$subnet_id
-    echo "Alterando Tag Name"
-    aws ec2 create-tags --resources $subnet_id --tags Key=Name,Value="Subrede Publica 03"
-    aws ec2 associate-route-table --subnet-id $subnet_id --route-table-id $public_rtb_id
+    echo "Criando subrede publica 03 - 10.50.4.0/24 - us-east-1c"
+    subnetid_4=$(aws ec2 create-subnet --vpc-id $vpcid --cidr-block 10.50.4.0/24 --availability-zone us-east-1c --output text --query 'Subnet.SubnetId')
+    echo SubnetId=$subnetid_4
+    echo "Alterando Tag Name da subrede para 'Subrede Publica 03'"
+    aws ec2 create-tags --resources $subnetid_4 --tags Key=Name,Value="Subrede Publica 03"
+    echo "Associando tabela de roteamento publica $public_routetableid a subrede $subnetid_4"
+    aws ec2 associate-route-table --subnet-id $subnetid_4 --route-table-id $public_routetableid
+
+echo =================================================================================
 
 # Subrede Privada 03 - 10.50.5.0/24 - zona de disponibilidade us-east-1c
-    subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.50.5.0/24 --availability-zone us-east-1c --output text --query 'Subnet.SubnetId')
-    echo SubnetId=$subnet_id
-    echo "Alterando Tag Name"
-    aws ec2 create-tags --resources $subnet_id --tags Key=Name,Value="Subrede Privada 03"
-    aws ec2 associate-route-table --subnet-id $subnet_id --route-table-id $private_rtb_id
+    echo "Criando subrede privada 01 - 10.50.5.0/24 - us-east-1c"
+    subnetid_5=$(aws ec2 create-subnet --vpc-id $vpcid --cidr-block 10.50.5.0/24 --availability-zone us-east-1c --output text --query 'Subnet.SubnetId')
+    echo SubnetId=$subnetid_5
+    echo "Alterando Tag Name da subrede para 'Subrede Privada 03'"
+    aws ec2 create-tags --resources $subnetid_5 --tags Key=Name,Value="Subrede Privada 03"
+    echo "Associando tabela de roteamento privada $private_routetableid a subrede $subnetid_5"
+    aws ec2 associate-route-table --subnet-id $subnetid_5 --route-table-id $private_routetableid
+
+echo =================================================================================
+
+# Criando grupos de seguranca para as instancias NAT, localizadas nas subredes publicas, liberando acesso para as subredes privadas acessarem a internet
+    echo "Criando grupo de seguranca para instancia NAT, na subrede publica 01/us-east-1a, liberando acesso para a subrede privada 01"
+    security_group_NAT1=$(aws ec2 create-security-group --group-name "Grupo de Seguranca NAT - us-east-1a" --description "Regras de Seguranca para a instancia NAT" --vpc-id $vpcid --query 'GroupId')
+    aws ec2 authorize-security-group-ingress --group-id $security_group_NAT1 --protocol -1 --port -1 --cidr 10.50.1.0/24
+
+echo =================================================================================
+    echo "Criando grupo de seguranca para instancia NAT, na subrede publica 02/us-east-1b, liberando acesso para a subrede privada 02"
+    security_group_NAT3=$(aws ec2 create-security-group --group-name "Grupo de Seguranca NAT - us-east-1b" --description "Regras de Seguranca para a instancia NAT" --vpc-id $vpcid --query 'GroupId')
+    aws ec2 authorize-security-group-ingress --group-id $security_group_NAT3 --protocol -1 --port -1 --cidr 10.50.3.0/24
+
+echo =================================================================================
+    echo "Criando grupo de seguranca para instancia NAT, na subrede publica 03/us-east-1c, liberando acesso para a subrede privada 03"
+    security_group_NAT5=$(aws ec2 create-security-group --group-name "Grupo de Seguranca NAT - us-east-1c" --description "Regras de Seguranca para a instancia NAT" --vpc-id $vpcid --query 'GroupId')
+    aws ec2 authorize-security-group-ingress --group-id $security_group_NAT5 --protocol -1 --port -1 --cidr 10.50.5.0/24
+
+echo =================================================================================
+
+# Dump das variaveis de ambiente, util para outros scripts
+echo export vpcname=$vpcname
+echo export vpcid=$vpcid
+echo export internetgatewayid=$internetgatewayid
+echo export public_routetableid=$public_routetableid
+echo export private_routetableid=$private_routetableid
+echo export subnetid_0=$subnetid_0
+echo export subnetid_1=$subnetid_1
+echo export subnetid_2=$subnetid_2
+echo export subnetid_3=$subnetid_3
+echo export subnetid_4=$subnetid_4
+echo export subnetid_5=$subnetid_5
+echo export security_group_NAT1=$security_group_NAT1
+echo export security_group_NAT3=$security_group_NAT3
+echo export security_group_NAT5=$security_group_NAT5
